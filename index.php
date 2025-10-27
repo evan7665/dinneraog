218aba76793f5e3203106adf6f9a4bce4652130f
evan7665 <51441749+evan7665@users.noreply.github.com>
2025-10-27 09:11:06 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,318 @@
+<?php
+$nama=$_GET['nama'];
+?>
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Christmas Dinner</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Merry christmas everyone hope you like this letter" />
+	<meta name="keywords" content="" />
+	<meta name="author" content="" />
+
+  <!-- 
+	//////////////////////////////////////////////////////
+
+	FREE HTML5 TEMPLATE 
+	DESIGNED & DEVELOPED by FREEHTML5.CO
+		
+	Website: 		http://freehtml5.co/
+	Email: 			info@freehtml5.co
+	Twitter: 		http://twitter.com/fh5co
+	Facebook: 		https://www.facebook.com/fh5co
+
+	//////////////////////////////////////////////////////
+	 -->
+
+  	<!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link href='https://fonts.googleapis.com/css?family=Work+Sans:400,300,600,400italic,700' rel='stylesheet' type='text/css'>
+	<link href="https://fonts.googleapis.com/css?family=Sacramento" rel="stylesheet">
+	
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+
+	<!-- Magnific Popup -->
+	<link rel="stylesheet" href="css/magnific-popup.css">
+
+	<!-- Owl Carousel  -->
+	<link rel="stylesheet" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="css/style.css">
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+	</head>
+	<body>
+		
+	<audio autoplay loop >
+  <source src="audio.mp3" type="audio/mpeg">
+  Your browser does not support the audio element.
+</audio>
+
+	
+	<div id="page">
+	
+
+	<header id="fh5co-header" class="fh5co-cover" role="banner" style="background-image:url(images/nyobalagi.jpeg); max-width:100%;height:auto;" data-stellar-background-ratio="0.5">
+
+		<div class="overlay"></div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2 text-center">
+					<div class="display-t">
+						<div class="display-tc animate-box" data-animate-effect="fadeIn">
+							<h1>Merry Christmas <?php echo $nama; ?></h1>
+							<h2>You're invited to Christmas Dinner! Join us for good food, great community, and holiday cheer. Your presence will make it even more special. See you there! </h2>
+							<div class="simply-countdown simply-countdown-one"></div>
+							
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+	
+
+	
+
+
+	
+
+	<div id="fh5co-testimonial">
+		<div class="container">
+			<div class="row">
+				<div class="row animate-box">
+					<div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
+						<span>quotes</span>
+						<h2>Bible Quotes</h2>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-12 animate-box">
+						<div class="wrap-testimony">
+							<div class="owl-carousel-fullwidth">
+								<div class="item">
+									<div class="testimony-slide active text-center">
+										
+										<span>John 3:16 <a href="#" class="twitter"></a></span>
+										<blockquote>
+											<p>"For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life."</p>
+										</blockquote>
+									</div>
+								</div>
+								<div class="item">
+									<div class="testimony-slide active text-center">
+										
+										<span>Ephesians 4:32<a href="#" class="twitter"></a></span>
+										<blockquote>
+											<p>"And be ye kind one to another, tenderhearted, forgiving one another, even as God for Christ's sake hath forgiven you."</p>
+										</blockquote>
+									</div>
+								</div>
+								<div class="item">
+									<div class="testimony-slide active text-center">
+										
+										<span>Romans 15:13<a href="#" class="twitter"></a></span>
+										<blockquote>
+											<p>"Now the God of hope fill you with all joy and peace in believing, that ye may abound in hope, through the power of the Holy Ghost."</p>
+										</blockquote>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div id="fh5co-services" class="fh5co-section-gray">
+		<div class="container">
+			
+			<div class="row animate-box">
+				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
+					<h2>The Promise of Christmas</h2>
+					<p>
+"The Promise of Christmas" typically symbolizes hope, joy, and the birth of Jesus Christ, emphasizing the themes of love, peace, and salvation. It embodies the spirit of giving and coming together in celebration, reflecting the essence of the holiday season.</p>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-md-6">
+					<div class="feature-left animate-box" data-animate-effect="fadeInLeft">
+						<span class="icon">
+							<i><img src="location.png" style="height: 40px; width: 40px; " ></i>
+						</span>
+						<div class="feature-copy">
+							<h3>Location And Date</h3>
+							<p>"Our event will begin at Bento Coffee in Semarang on Sunday, December 10th, 2023, at 5 PM. Click the button below to get the link for the address."</p>
+							<p><a href="https://www.google.com/maps/place/Bento+Kopi+Semarang/@-7.0587246,110.4694433,17z/data=!3m1!4b1!4m6!3m5!1s0x2e7a7daab530140f:0xc366353c0e7bce01!8m2!3d-7.0587246!4d110.4720182!16s%2Fg%2F11sgcw048s?hl=in-ID&entry=ttu" class="btn btn-default btn-sm">Open Map </a></p>
+						</div>
+					</div>
+
+					<div class="feature-left animate-box" data-animate-effect="fadeInLeft">
+						<span class="icon">
+							<i><img src="dress2.png" style="height: 40px; width: 40px; " ></i>
+						</span>
+						<div class="feature-copy">
+							<h3>Dress Code</h3>
+							<img src="coba lousa.png" alt="woi ngetes boi" style="width: 300px; height: 100px; " >
+						</div>
+					</div>
+
+					<div class="feature-left animate-box" data-animate-effect="fadeInLeft">
+						<span class="icon">
+							<i><img src="gift1.png" style="height: 30px; width: 30px; " ></i>
+						</span>
+						<div class="feature-copy">
+							<h3>Gift</h3>
+							<div>
+								<img src="8.png" alt="" style="width: 70px; height: 70px; " >   
+								<p>Wrap the gift with newspaper.</p>
+							</div>
+							<div>
+								<img src="9.png" alt="" style="width: 70px; height: 70px; " >
+								<p>Price range: 15-20K IDR.</p>
+							</div>
+							<div>
+								<img src="10.png" alt="" style="width: 70px; height: 70px; " >
+								<p>No food and drink.</p>
+							</div>
+						</div>
+					</div>
+
+					<div class="feature-left animate-box" data-animate-effect="fadeInLeft">
+						<span class="icon">
+							<i><img src="link (1).png" style="height: 30px; width: 30px;" ></i>
+						</span>
+						<div class="feature-copy">
+							<h3>Registration</h3>
+							<p><a href="https://forms.gle/U4Du7nuWxYABnaqdA" class="btn btn-default btn-sm">click me for registration </a></p>
+						</div>
+					</div>
+
+				</div>
+
+				<div class="col-md-6 animate-box">
+					<div class="fh5co-video fh5co-bg" style="background-image: url(images/img_bg_3.jpg); ">
+						<a href="KONTEN CD PART 2.mp4" class="popup-vimeo"><i class="icon-video2"></i></a>
+						<div class="overlay"></div>
+					</div>
+				</div>
+			</div>
+
+			
+		</div>
+	</div>
+
+
+	
+
+	<footer id="fh5co-footer" role="contentinfo">
+		<div class="container">
+			<div class="row copyright">
+				<div class="col-md-12 text-center">
+					<p>
+						<small class="block"><img src="Dress code .png" alt="test" style="width: 15px; height: 15px; " > Made with love <img src="Dress code .png" alt="test" style="width: 15px; height: 15px; " ></small> 
+						<small class="block">Designed by <a target="_blank">Coach Alive</a></small>
+					</p>
+				</div>
+			</div>
+		</div>
+	</footer>
+	</div>
+
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up"></i></a>
+	</div>
+	
+	<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Carousel -->
+	<script src="js/owl.carousel.min.js"></script>
+	<!-- countTo -->
+	<script src="js/jquery.countTo.js"></script>
+
+	<!-- Stellar -->
+	<script src="js/jquery.stellar.min.js"></script>
+	<!-- Magnific Popup -->
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="js/magnific-popup-options.js"></script>
+
+	<!-- // <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/0.0.1/prism.min.js"></script> -->
+	<script src="js/simplyCountdown.js"></script>
+	<!-- Main -->
+	<script src="js/main.js"></script>
+
+	<script>
+    // Mengambil tanggal dan waktu saat ini
+    // Mengambil tanggal dan waktu saat ini
+var currentDate = new Date();
+
+// Menambah 5 hari ke tanggal saat ini
+var targetDate = new Date(currentDate);
+targetDate.setDate(currentDate.getDate() + 5); // Mengubah ini dari +6 menjadi +5
+
+// Set jam menjadi 17:00 (5 sore)
+targetDate.setHours(17, 0, 0, 0);
+
+// default example
+simplyCountdown('.simply-countdown-one', {
+    year: targetDate.getFullYear(),
+    month: targetDate.getMonth() + 1,
+    day: targetDate.getDate(),
+    hours: targetDate.getHours(),
+    minutes: targetDate.getMinutes(),
+    seconds: targetDate.getSeconds(),
+    enableUtc: false // Sesuaikan berdasarkan kebutuhan Anda
+});
+
+// jQuery example
+$('#simply-countdown-losange').simplyCountdown({
+    year: targetDate.getFullYear(),
+    month: targetDate.getMonth() + 1,
+    day: targetDate.getDate(),
+    hours: targetDate.getHours(),
+    minutes: targetDate.getMinutes(),
+    seconds: targetDate.getSeconds(),
+    enableUtc: false // Sesuaikan berdasarkan kebutuhan Anda
+});
+
+</script>
+
+	</body>
+</html>
+
